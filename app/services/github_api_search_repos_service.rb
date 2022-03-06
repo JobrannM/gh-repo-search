9d4cc0950ea3494407abf60ea6b189070babd9ff
JobrannM <jobrann.mous@hotmail.com>
2022-03-06 15:17:25 +0100

    add github api service

---
+++ app/services/github_api_search_repos_service.rb
@@ -0,0 +1,47 @@
+#frozen_string_literal: true
+
+class GithubApiSearchReposService
+  class GithubApiError < StandardError; end
+
+  BASE_URL = 'https://api.github.com'
+  ENDPOINT = '/search/repositories'
+  SORT_BY = 'stars'
+  RESULTS_PER_PAGE = 100
+
+  attr_reader :search_term
+
+  def initialize(search_term)
+    @search_term = search_term
+  end
+
+  def perform
+    response = HTTParty.get(
+      url,
+      headers
+    )
+
+    if response.success?
+      JSON.parse(response.body)
+    else
+      raise GithubApiError.new("HTTP request failed with code #{response.code}")
+    end
+  end
+
+  private
+
+  def url
+    BASE_URL +
+    ENDPOINT + '?&' +
+    "q=#{search_term}" + '&' +
+    "sort=#{SORT_BY}" + '&'+
+    "per_page=#{RESULTS_PER_PAGE}"
+  end
+
+  def headers
+    {
+      headers: {
+        "Accept" => "application/vnd.github.v3+json"
+      }
+    }
+  end
+end
---
+++ spec/services/github_api_search_repos_service_spec.rb
@@ -0,0 +1,30 @@
+require 'rails_helper'
+
+RSpec.describe GithubApiSearchReposService do
+  let(:search_term) { 'adjust analytics' }
+
+  subject { described_class.new(search_term) }
+
+  describe "#perform" do
+    context 'successful call' do
+      it "succeeds"do 
+        VCR.use_cassette('github_api_search_repos_service') do
+          expect{ subject.perform }.to_not raise_error
+        end
+      end
+    end
+
+    context 'failed call' do
+      before do
+        stub_request(:get, subject.send(:url))
+          .to_return(status: [500, "stubbed error"])
+      end
+
+      it "returns a custom error" do
+        VCR.use_cassette('github_api_search_repos_service') do
+          expect{ subject.perform }.to raise_error(GithubApiSearchReposService::GithubApiError)
+        end
+      end
+    end
+  end
+end
