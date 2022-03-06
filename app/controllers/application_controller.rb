4ab821eb3c10d8bbb302ce6542aa801ae24ca593
JobrannM <jobrann.mous@hotmail.com>
2022-03-06 17:47:55 +0100

    add basic error handling on controller level

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,14 @@
 class ApplicationController < ActionController::Base
+  rescue_from StandardError, with: :internal_server_error
+  rescue_from GithubApiSearchReposService::GithubApiError, with: :service_unavailable
+
+  def internal_server_error
+    response = { error: 'Internal Server Error' }
+    render json: response, status: :internal_server_error
+  end
+
+  def service_unavailable
+    response = { error: 'Service Unavailable' }
+    render json: response, status: :service_unavailable
+  end
 end
